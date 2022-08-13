1f7ec7901895cda23b830692ae44ae870d872bfb
Grant Wiswell <gt101boy@gmail.com>
2022-08-13 14:13:34 -0500

    Created faq and "our story"

---
+++ src/routes/faq.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { dummyQuestions, questionList } from "$lib/stores/questionStore";
+</script>
+
+<div class="flex w-full justify-center">
+<div class="flex flex-col justify-center align-middle p-16 w-fit">
+	{#each dummyQuestions as question}
+    <div>
+		<div
+			tabindex="0"
+			class="collapse collapse-arrow border border-base-300 bg-neutral rounded-box"
+		>
+			<div class="collapse-title text-xl font-medium">{question.question}</div>
+			<div class="collapse-content">
+				<p>{question.answer}</p>
+			</div>
+		</div>
+        <div class="divider m-0"></div>
+    </div>
+	{/each}
+</div>
+</div>
---
+++ src/routes/story.svelte
@@ -0,0 +1,15 @@
+<div class="card m-12 self-center bg-primary text-primary-content">
+	<div class="card-body">
+		<h2 class="card-title place-self-center">Our Story</h2>
+		<p class="mt-4">
+			We are a humble thingy and we pride ourselves on being really cool all of the time!We are a
+			humble thingy and we pride ourselves on being really cool all of the time!We are a humble
+			thingy and we pride ourselves on being really cool all of the time!We are a humble thingy and
+			we pride ourselves on being really cool all of the time!We are a humble thingy and we pride
+			ourselves on being really cool all of the time!We are a humble thingy and we pride ourselves
+			on being really cool all of the time!We are a humble thingy and we pride ourselves on being
+			really cool all of the time!We are a humble thingy and we pride ourselves on being really cool
+			all of the time!
+		</p>
+	</div>
+</div>
