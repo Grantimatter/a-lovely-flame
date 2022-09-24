aa482ed913c1a03637b3a4394aedeba2ed7bac34
Grant Wiswell <wiswellgrant@gmail.com>
2022-09-23 19:20:28 -0500

    Added great vibes font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 		<div class="hero">
 			<div class="hero-content text-center">
 				<div class="max-w-md">
-					<h1 class="text-5xl font-bold">Hello There!</h1>
+					<h1 class="text-5xl font-bold" style="font-family: Great Vibes">Hello There!</h1>
 					<p class="py-6">This is a candle business and we're really cool!</p>
 					<a class="btn btn-primary" href="/products">Check out our products!</a>
 				</div>
