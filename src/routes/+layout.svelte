93ce4bea27a231254de2843ee99f8b8f6a98d6a2
Grant Wiswell <wiswellgrant@gmail.com>
2022-09-26 16:28:20 -0500

    Removed netlify functions and firebase stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,7 @@
 	import Header from '../lib/components/header.svelte';
 	import Footer from '../lib/components/footer.svelte';
 	import MainMenu from '../lib/components/navigation.svelte';
-
-	import { userStore } from '$lib/stores/userStore';
 	import { onMount } from 'svelte';
-
-	onMount( async () => {
-		await userStore.checkSignIn();
-	});
 </script>
 
 <div class="flex flex-col justify-between min-h-screen">
