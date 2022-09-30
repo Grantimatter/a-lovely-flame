5b6fefe3234ed609a4fc7e2a40f8a686e11a04b7
Grant Wiswell <wiswellgrant@gmail.com>
2022-09-29 19:30:01 -0500

    added cart store

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+    import { cartStore } from "$src/lib/stores/cartStore";
 </script>
 
 <div class="font-bold text-primary p-60 text-center text-5xl">You are at the cart!</div>
+
+<div>
+    {#each $cartStore.productList}
+    [/each]
+</div>
