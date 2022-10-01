6da12fca46682ecaf36a0f306de67b987414774a
Grant Wiswell <wiswellgrant@gmail.com>
2022-09-30 19:48:09 -0500

    fixed products page getting stuck

--- src/routes/products/[product_type]/+page.svelte
+++ src/routes/products/[product_type]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import { variables } from '$lib/variables';
-	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
-	import { productStore } from '$stores/productStore';
 	import ProductCard from '$src/lib/components/productCard.svelte';
 
 	export let data: PageData;
 
-	let attributes = data[0].attributes;
-	//$: attributes = data[0].attributes;
+	let attributes: any;
+
+	$: if (data && data[0]){
+		attributes = data[0].attributes;	
+	} 
 </script>
 
 <div class="m-12">
