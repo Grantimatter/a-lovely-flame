c3aa509c0aae260a1c0d5a015279ba3b524d9b15
Grant Wiswell <wiswellgrant@gmail.com>
2022-10-03 20:14:54 -0500

    corrected payment-success screen

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,4 +1,5 @@
 export const variables = {
     STRAPI_URL: import.meta.env.VITE_STRAPI_URL,
-    STRAPI_API_URL: import.meta.env.VITE_STRAPI_API_URL
+    STRAPI_API_URL: import.meta.env.VITE_STRAPI_API_URL,
+    HOST_URL: import.meta.env.VITE_HOST_URL
 };
--- src/routes/api/cart/checkout/+server.ts
+++ src/routes/api/cart/checkout/+server.ts
@@ -2,6 +2,7 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { getProductLineItems } from '$src/lib/utility/strapi/productsApi';
 import { STRIPE_SECRET_KEY } from '$env/static/private';
+import { variables } from '$lib/variables';
 import Stripe from 'stripe';
 const stripe = new Stripe(STRIPE_SECRET_KEY, {apiVersion: '2022-08-01'});
 
@@ -13,8 +14,8 @@ export const POST: RequestHandler = async ({request}): Promise<Response> => {
     const session = await stripe.checkout.sessions.create({
         line_items,
         mode: 'payment',
-        success_url: 'https://alovelyflame.com/payment-success',
-        cancel_url: 'https://alovelyflame.com/cart',
+        success_url: `${variables.HOST_URL}/payment-success`,
+        cancel_url: `${variables.HOST_URL}/cart`,
         allow_promotion_codes: true,
         shipping_address_collection: {allowed_countries: ['US']},
     });
